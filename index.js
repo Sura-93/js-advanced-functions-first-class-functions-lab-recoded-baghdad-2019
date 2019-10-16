804df21f2c557ff01782d3982bad82c1108d39d4
Sura Al-jilawi <suraaljailawi@gmail.com>
2019-10-16 19:24:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function createFareMultiplier (int) {
 }
 const fareDoubler =  createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
-function selectDifferentDrivers(drivers ) {
+function selectDifferentDrivers(drivers, selection ) {
   
   
 }
